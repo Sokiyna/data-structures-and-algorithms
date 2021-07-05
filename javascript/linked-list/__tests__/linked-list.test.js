64dcdaca0ceea804a391b6de51bf3a92cf094f8d
Sokiyna <sokiynanaser@yahoo.com>
2021-07-05 20:00:58 +0300

    finish lab 7 editfinal

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,101 +1,125 @@
 'use strict';
-
+// Require our linked list implementation
 const LinkedLilst = require('../index');
+describe('checks if it includes a value', () => {
+  test('insert', () => {
+    let ll = new LinkedLilst();
+    expect(ll.head).toBe(null);
+    ll.insert('a');
+    expect(ll.head.value).toBe('a');
+    ll.insert(false);
+    ll.insert('c');
+    ll.insert('d');
+  });
 
-describe('Linked List',()=>{
-  it('Should Instantiate',()=>{
+  test('includes', () => {
+    let ll = new LinkedLilst();
+    ll.insert('a');
+    ll.insert(false);
+    ll.insert('c');
+    ll.insert('d');
+    expect(ll.includes('d')).toBe(true);
+    expect(ll.includes('a')).toBe(true);
+    expect(ll.includes('h')).toBe(false);
+  });
+  test('read all node values', () => {
+    let ll = new LinkedLilst();
+    ll.insert('a');
+    ll.insert(false);
+    ll.insert('c');
+    ll.insert('d');
+    expect(ll.toString()).toBe('{d} -> {c} -> {false} -> {a}');
+  });
+  test(' add a node to the end ', () => {
     const ll = new LinkedLilst();
-    expect(ll).toBeDefined();
-    expect(ll.head).toBeNull();
+    ll.append('one');
+    ll.append('two');
+    expect(ll.head.next.value).toBe('two');
   });
-});
+  test(' add multiple nodes ', () => {
+    const ll = new LinkedLilst();
+    ll.append('one');
+    ll.append('two');
+    ll.append('three');
+    ll.append('four');
+    expect(ll.head.next.next.next.value).toBe('four');
+  });
 
-describe('can insert',()=>{
-  it('can insert head ', ()=>{
-    let ll = new LinkedLilst();
-    ll.insert('first');
-    expect(ll.head.value).toBe('first');      
-  });
-  it('multiple nodes',()=>{
-    let ll = new LinkedLilst();
-    ll.insert('first');
-    ll.insert('sec');
-    ll.insert('thir');
-    expect(ll.head.next).toBeDefined();
-  });
-  it('include node',()=>{
-    let ll = new LinkedLilst();
-    ll.insert('first');
-    ll.insert('sec');
-    ll.insert('thir');
-    expect(ll.includes('sec')).toBeTruthy();
-    expect(ll.includes('fourth')).toBeFalsy();
-  });
-  it('return a collection',()=>{
-    let ll = new LinkedLilst();
-    ll.insert('first');
-    ll.insert('sec');
-    ll.insert('thir');
-    expect(ll.toString()).toBe('{thir} -> {sec} -> {first}')
+  test('insert in the middle', () => {
+    const ll = new LinkedLilst();
+    ll.append('one');
+    ll.append('two');
+    ll.append('three');
+    ll.append('four');
+    ll.insertBefore('amro', 'three');
+    expect(ll.head.next.next.value).toBe('amro');
   })
+  test('insert before first', () => {
+    const ll = new LinkedLilst();
+    ll.append('one');
+    ll.append('two');
+    ll.append('three');
+    ll.append('fourth');
+    ll.insertBefore('amro', 'one');
+    expect(ll.head.value).toBe('amro');
+  })
+  test('insert after a value', () => {
+    const ll = new LinkedLilst();
+    ll.append('one');
+    ll.append('two');
+    ll.append('three');
+    ll.append('four');
+    ll.insertAfter('amro', 'three');
+    expect(ll.head.next.next.next.value).toBe('amro');
+  })
+  test('insert after last', () => {
+    const ll = new LinkedLilst();
+    ll.append('one');
+    ll.append('two');
+    ll.append('three');
+    ll.append('four');
+    ll.insertAfter('amro', 'four');
+    expect(ll.head.next.next.next.next.value).toBe('amro');
+  })
+  test('Where k is greater than the length of the linked list', () => {
+    const ll = new LinkedLilst();
+    ll.append('one');
+    ll.append('two');
+    ll.append('three');
+    ll.append('four');
+    ll.insertAfter('amro', 'four');
+    expect(ll.kthFromEnd(9)).toBe('Exception');
+  })
+  test('Where k and the length of the list are the same', () => {
+    const ll = new LinkedLilst();
+    ll.append('one');
+    ll.append('two');
+    ll.append('three');
+    ll.append('four');
+    ll.insertAfter('amro', 'four');
+    expect(ll.kthFromEnd(5)).toBe('Exception');
+  })
+  test('Where k is not a positive integer', () => {
+    const ll = new LinkedLilst();
+    ll.append('one');
+    ll.append('two');
+    ll.append('three');
+    ll.append('four');
+    ll.insertAfter('amro', 'four');
+    expect(ll.kthFromEnd(-5)).toBe('Exception');
+  })
+  test('Where the linked list is of a size 1', () => {
+    const ll = new LinkedLilst();
+    ll.append('first');
+    expect(ll.kthFromEnd(0)).toBe('first');
+  });
+  test('Happy Path” where k is not at the end, but somewhere in the middle of the linked list', () => {
+    const ll = new LinkedLilst();
+    ll.append('first');
+    ll.append('sec');
+    ll.append('thir');
+    ll.append('fourth');
+    expect(ll.kthFromEnd(2)).toBe('sec');
+  });
+
 })
-
-
-describe('linked-list-insertions',()=>{
-
-  it(' add a node to the end ',()=>{
-    const ll = new LinkedLilst();
-    ll.append('first');
-    ll.append('sec');
-    expect(ll.head.next.value).toBe('sec');
-  });
-
-  it(' add multiple nodes ',()=>{
-    const ll = new LinkedLilst();
-    ll.append('first');
-    ll.append('sec');
-    ll.append('thir');
-    ll.append('fourth');
-    expect(ll.head.next.next.next.value).toBe('fourth');
-  });
-
-
-
-  test('insert in the middle',()=>{
-    const ll = new LinkedLilst();
-    ll.append('first');
-    ll.append('sec');
-    ll.append('thir');
-    ll.append('fourth');
-    ll.insertBefore('stuff','thir');
-    expect(ll.head.next.next.value).toBe('stuff');
-  })
-  test('insert before first',()=>{
-    const ll = new LinkedLilst();
-    ll.append('first');
-    ll.append('sec');
-    ll.append('thir');
-    ll.append('fourth');
-    ll.insertBefore('stuff','first');
-    expect(ll.head.value).toBe('stuff');
-  })
-  test('insert after a value',()=>{
-    const ll = new LinkedLilst();
-    ll.append('first');
-    ll.append('sec');
-    ll.append('thir');
-    ll.append('fourth');
-    ll.insertAfter('stuff','thir');
-    expect(ll.head.next.next.next.value).toBe('stuff');
-  })
-  test('insert after last',()=>{
-    const ll = new LinkedLilst();
-    ll.append('first');
-    ll.append('sec');
-    ll.append('thir');
-    ll.append('fourth');
-    ll.insertAfter('stuff','fourth');
-    expect(ll.head.next.next.next.next.value).toBe('stuff');
-  })
-
-});
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,109 +1,127 @@
 'use strict';
+
+
 class Node {
-  constructor(value, next = null) {
-    this.value = value;
-    this.next = next;
-  }
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
 }
 class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-  insert(value) {
-
-    try {
-      let addNode = new Node(value);
-      if (!this.head) {
-        this.head = addNode;
-      } else {
-        addNode.next = this.head;
-        this.head = addNode;
-      }
-    } catch (err) {
-      console.error(err)
+    constructor() {
+        this.head = null;
+        this.counter = 0;
     }
+    insert(value) {
+        this.counter = this.counter + 1;
+        let addNode = new Node(value);
+        if (!this.head) {
+            this.head = addNode;
+        } else {
+            addNode.next = this.head;
+            this.head = addNode;
+        }
+    }
+    includes(value) {
 
-  }
-  includes(value) {
-    try {
-      let current = this.head;
-      if (!this.head) {
+        let current = this.head;
+        if (!this.head) {
+            return false;
+        }
+        while (current.next) {
+            if (current.value == value) {
+                return true;
+            }
+            current = current.next;
+            if ((current.next == null) && (value == current.value)) {
+                return true;
+            }
+        }
         return false;
-      }
-      while (current.next) {
-        if (current.value == value) {
-          return true;
+    }
+    toString() {
+        let outPut = '';
+        let current = this.head;
+        try {
+
+            while (current.next) {
+                if (current.value == null || current.value == undefined) {
+                    outPut = outPut + `NULL -> `;
+                } else {
+                    outPut = outPut + `{${current.value}} -> `;
+                }
+                current = current.next;
+                if (current.next == null) {
+                    outPut = outPut + `{${current.value}}`;
+                }
+            }
+            return outPut;
+        } catch (e) {
+            console.log('error...');
         }
-        current = current.next;
-        if ((current.next == null) && (value == current.value)) {
-          return true;
+    }
+    append(value) {
+        this.counter = this.counter + 1;
+        let addNode = new Node(value);
+        if (!this.head) {
+            this.head = addNode;
+        } else {
+            let currnt = this.head;
+            while (currnt.next) {
+                currnt = currnt.next;
+            }
+            currnt.next = addNode
         }
-      }
-      return false;
-    } catch (err) {
-      console.error(err)
     }
 
-  }
-  toString() {
-    let outPut = '';
-    let current = this.head;
-    while (current.next) {
-      if (current.value == null || current.value == undefined) {
-        outPut = outPut + `NULL -> `;
-      } else {
-        outPut = outPut + `{${current.value}} -> `;
-      }
-      current = current.next;
-      if (current.next == null) {
-        outPut = outPut + `{${current.value}}`;
-      }
-    }
-    return outPut;
-  }
-
-  append(value) {
-    let node1 = new Node(value);
-    if (!this.head) {
-      this.head = node1;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = node1;
-    }
-  }
-  insertAfter(newValue, value) {
-    let node1 = new Node(newValue);
-    let current = this.head;
-    while (current) {
-      if (current.value == value) {
-        node1.next = current.next;
-        current.next = node1;
-        break;
-      }
-      console.log(current.value);
-      current = current.next;
-    }
-  }
-  insertBefore(newValue, value) {
-    let node1 = new Node(newValue);
-    let current = this.head;
-    if (value == this.head.value) {
-      node1.next = this.head;
-      this.head = node1;
-    } else {
-      while (current) {
-        if (current.next.value == value) {
-          node1.next = current.next;
-          current.next = node1;
-          break;
+    insertAfter(newValue, value) {
+        this.counter = this.counter + 1;
+        let addNew = new Node(newValue);
+        let current = this.head;
+        while (current) {
+            if (current.value == value) {
+                addNew.next = current.next;
+                current.next = addNew;
+                break;
+            }
+            console.log(current.value);
+            current = current.next;
         }
-        console.log(current.value);
-        current = current.next;
-      }
     }
-  }
+
+    insertBefore(newValue, value) {
+        this.counter = this.counter + 1;
+        let addNew = new Node(newValue);
+        let current = this.head;
+        if (value == this.head.value) {
+            addNew.next = this.head;
+            this.head = addNew;
+        } else {
+            while (current) {
+                if (current.next.value == value) {
+                    addNew.next = current.next;
+                    current.next = addNew;
+                    break;
+                }
+                console.log(current.value);
+                current = current.next;
+            }
+        }
+    }
+    kthFromEnd(k) {
+        let current = this.head;
+        let cnt = 1;
+        if (k >= this.counter || k < 0) {
+            return 'Exception';
+        }
+        while (current) {
+            if (cnt == this.counter - k) {
+                return current.value;
+            }
+            cnt = cnt + 1;
+            current = current.next;
+        }
+    }
+
 }
 module.exports = LinkedList;
